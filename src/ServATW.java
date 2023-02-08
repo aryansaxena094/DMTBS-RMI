1836035dd535a365b73b630314546e4a4c29cef9
aryansaxena094 <aryan.saxena094@gmail.com>
2023-02-08 12:38:59 -0500

    commit

--- src/ServATW.java
+++ src/ServATW.java
@@ -9,10 +9,10 @@ import java.util.*;
 public class ServATW extends UnicastRemoteObject implements RMIs {
     private String ServerName = "Atwater";
     ArrayList<String> admin = new ArrayList<String>();
-
+    
     private HashMap<String, HashMap<String, Integer>> movies = new HashMap<>();
     private HashMap<String, Integer> moviecapacity = new HashMap<>();
-
+    
     private HashMap<String, HashMap<String, Integer>> customer = new HashMap<>();
     private HashMap<String, Integer> moviesbookedbycustomer = new HashMap<>();
     
@@ -31,27 +31,74 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
     
     @Override
     public String addMovieSlots(String movieID, String movieName, int bookingcapacity) throws RemoteException {
-        boolean movieexists = moviecapacity.containsKey(movieID);
-        moviecapacity.merge(movieID, bookingcapacity,Integer::sum);
-        movies.put(movieName, moviecapacity);
-        
-        if(movieexists==true){
-            return bookingcapacity+" capacity has been added to "+movieName+" with Movie ID " +movieID;
+
+        boolean movieExists = movies.containsKey(movieName);
+        if (movieExists) {
+            HashMap<String, Integer> movieCapacity = movies.get(movieName);
+            boolean movieIDExists = movieCapacity.containsKey(movieID);
+            if (movieIDExists) {
+                movieCapacity.merge(movieID, bookingcapacity, Integer::sum);
+            } else {
+                movieCapacity.put(movieID, bookingcapacity);
+            }
+            movies.put(movieName, movieCapacity);
+        } else {
+            HashMap<String, Integer> movieCapacity = new HashMap<>();
+            movieCapacity.put(movieID, bookingcapacity);
+            movies.put(movieName, movieCapacity);
         }
+
+
+        
+        // boolean movieexists = movies.containsKey(movieName);
+        // if(movieexists==true){
+        //     boolean movieIDexists = (movies.get(movieName)).containsKey(movieID);
+        //     if(movieIDexists==true)
+        //     {
+        //         (movies.get(movieName)).merge(movieID, bookingcapacity,Integer::sum);
+        //     }
+        // }
+        
+        // moviecapacity.put(movieID, bookingcapacity);
+        // movies.put(movieName, moviecapacity);
+
+        //FIX MULTIPLE MOVIE THING
+        //MERGE FUNCTION HAS PROBLEM
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
+        System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
+        System.out.println("Hashmap with Customer Values: "+ customer.toString());
+        ///
+        
+        
+        
+        
+        // if(movieexists==true){
+        //     return bookingcapacity+" capacity has been added to "+movieName+" with Movie ID " +movieID;
+        // }
         return movieName+" with MovieID "+ movieID +" has been created with "+bookingcapacity + " capacity";
     }
     
     @Override
     public String removeMovieSlots(String movieID, String movieName) throws RemoteException {
-        boolean movieexists = moviecapacity.containsKey(movieID);
-        if(movieexists==false){
-            return movieName+" with MovieID "+movieID +" does not exist!";
+
+        boolean movieexists = movies.containsKey(movieName);
+        if(movieexists==true){
+            boolean movieIDexists = (movies.get(movieName)).containsKey(movieID);
+            if(movieIDexists==true){
+                (movies.get(movieName)).remove(movieID);
+            }
         }
-        moviecapacity.remove(movieID);
-        movies.put(movieName, moviecapacity);
+        
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
+        System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
+        System.out.println("Hashmap with Customer Values: "+ customer.toString());
+        ///
+        
         return movieName+" with MovieID "+movieID +" has been removed";
-        
-        
         
         //if while removing customers have booked then scenario!!
     }
@@ -63,12 +110,20 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
             return "No shows with this Movie Name";
         }
         ArrayList<String> listallshows = new ArrayList<String>();
-        Iterator it = moviecapacity.entrySet().iterator();
+        Iterator it = (movies.get(movieName)).entrySet().iterator();
         while(it.hasNext()){
             Map.Entry pair = (Map.Entry) it.next();
             listallshows.add(pair.getKey()+" with "+pair.getValue()+" capacity");
             it.remove();
         }
+        
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
+        System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
+        System.out.println("Hashmap with Customer Values: "+ customer.toString());
+        ///
+        
         return listallshows.toString();
     }
     
@@ -82,7 +137,7 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
         if(Numberoftickets>totaltickets){
             return "Lesser slots availabe, you can only book "+totaltickets+" for this show";
         }
-
+        
         totaltickets = totaltickets - Numberoftickets;
         moviesbookedbycustomer.merge(movieID, Numberoftickets, Integer::sum);
         customer.put(CustomerID, moviesbookedbycustomer);
@@ -90,10 +145,16 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
         //updaing movies hashmap
         moviecapacity.put(movieID, totaltickets);
         movies.put(movieName, moviecapacity);
-        ///
+        
+        
+        
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
         System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
         System.out.println("Hashmap with Customer Values: "+ customer.toString());
         ///
+        
         return Numberoftickets+ " Tickets have been booked to "+movieID+" show of "+movieName;
     }
     
@@ -103,15 +164,28 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
         {
             return "No shows booked in "+ServerName;
         }
-
+        
         Iterator it = (customer.get(CustomerID)).entrySet().iterator();
         ArrayList<String> allbookedshows = new ArrayList<>();
-
+        
         while(it.hasNext()){
             Map.Entry pair = (Map.Entry) it.next();
             allbookedshows.add(pair.getValue()+" tickets for "+ pair.getKey());
             it.remove();
         }
+        
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
+        System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
+        System.out.println("Hashmap with Customer Values: "+ customer.toString());
+        ///
+        
+        
+        
+        
+        
+        
         return allbookedshows.toString();
     }
     
@@ -126,17 +200,23 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
         moviesbookedbycustomer.put(movieID, ticketsbookedbycustomer);
         
         (movies.get(movieName)).put(movieID, ((movies.get(movieName)).get(movieID)) - Numberoftickets);
+        
+        ///testing
+        System.out.println("Movies Hashmap: "+movies.toString());
+        System.out.println("Movies Capacity Hashmap: "+moviecapacity.toString());
+        System.out.println("Hashmap with MovieID & tickets: "+ moviesbookedbycustomer.toString());
+        System.out.println("Hashmap with Customer Values: "+ customer.toString());
+        ///
+        
+        
+        
+        
         return Numberoftickets+" tickets has been cancelled for "+movieName+" with MovieID "+movieID;
     }
-
-
-
-
-
-
+    
     @Override
     public String verifyID(String ID) throws RemoteException{
-
+        
         if(ID.length() < 8 || ID.charAt(3)!='A' &&  ID.charAt(3)!='C' && ID.charAt(3)!='a' && ID.charAt(3)!='c')
         {
             return "invalid";
@@ -149,13 +229,24 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
                 return "admin";
             }
         }
-
-        return "customer";
+        
+        if(ID.charAt(3)=='C' || ID.charAt(3)=='c'){
+            //admincheck
+            if(admin.contains(ID)){
+                //checking if admin exists in db
+                return "customer";
+            }
+        }
+        return "invalid";
     }
-
+    
     @Override
     public String verifyMovieID(String movieID) throws RemoteException{
-        // TODO Auto-generated method stub
+        //verifying the movie ID
+        
+        
+        
+        
         return null;
     }
 }
