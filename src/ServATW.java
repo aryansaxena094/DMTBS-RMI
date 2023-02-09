a738798c88ab26e02aa022bad5dc9efa7a37a258
aryansaxena094 <aryan.saxena094@gmail.com>
2023-02-09 08:02:23 -0500

    commit and sync

--- src/ServATW.java
+++ src/ServATW.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,20 +11,66 @@ import java.util.*;
 
 
 public class ServATW extends UnicastRemoteObject implements RMIs {
-    private String ServerName = "Atwater";
+    private static String ServerName = "Atwater";
     ArrayList<String> admin = new ArrayList<String>();
-    private HashMap<String, HashMap<String, Integer>> movies = new HashMap<>();
-    private HashMap<String, HashMap<String, Integer>> customer = new HashMap<>();
+    private static HashMap<String, HashMap<String, Integer>> movies = new HashMap<>();
+    private static HashMap<String, HashMap<String, Integer>> customer = new HashMap<>();
+    
+    //own port
+    static int alwaysonport = 9999;
+    static int dataincoming = 1234;
+    static int dataoutgoing = 2345;
+    
+    //server1 ports
+    static int serv1alwaysonport = 9999;
+    static int serv1dataincoming = 1222;
+    static int serv1dataoutgoing = 1222;
+    
+    //datafrom server2 ports
+    static int serv2alwaysonport = 9999;
+    static int serv2dataincoming = 7777;
+    static int serv2dataoutgoing = 8593;
 
+    
     protected ServATW() throws RemoteException {
         super();
         admin.add("ATWA9499");
     }
     
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException, IOException {
         Registry reg = LocateRegistry.createRegistry(5099);
         reg.bind("ATW", new ServATW());
         System.out.println("Atwater Server is running!");
+        
+
+
+
+
+
+
+        try (//UDP Server OPEN PORT ALWAYS OPEN
+        DatagramSocket ATWSocket = new DatagramSocket(alwaysonport)) 
+        {
+            byte[] receivedata = new byte[1024];
+            
+            //Keeping UDP Port Open for Recieving
+            while(true){
+                DatagramPacket receivePacket = new DatagramPacket(receivedata, receivedata.length);
+                ATWSocket.receive(receivePacket);
+                String recieved = receivePacket.toString();
+                
+                //sample recieved: LATWAVATAR
+                if(recieved.charAt(0)=='L' || recieved.charAt(0)=='l'){
+                    String curmoviename = recieved.substring(4);
+                    String requestfromserver = recieved.substring(1,4);
+                    
+                    listMovieServertoServer(curmoviename,requestfromserver);
+                }
+                else if(recieved.charAt(0)=='C' || recieved.charAt(0)=='c'){
+                    //cancellation open  communication link
+                }
+            }
+        }
     }
     
     @Override
@@ -63,18 +113,73 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
         //if customers have booked scenario
     }
     
+    
+    
+    
+    
+    
     @Override
     public String listMovieShows(String movieName) throws RemoteException {
-        boolean movieexists = movies.containsKey(movieName);
-        if(movieexists==false){
-            return "No shows with this Movie Name";
-        }
+        
         ArrayList<String> listallshows = new ArrayList<String>();
         Map<String, Integer> tempMap = new HashMap<>(movies.get(movieName));
+        String output = "";
+
+        //for own server
         for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
             listallshows.add(entry.getKey() + " with " + entry.getValue() + " capacity");
         }
-        return listallshows.toString();
+        
+        //checking other servers
+        try {
+            String sendingrequest = "L"+"ATW"+movieName;
+            byte[] senddata = new byte[1024];
+            senddata = sendingrequest.getBytes();
+            
+            byte[] receivedataserv1 = new byte[1024];
+            byte[] receivedataserv2 = new byte[1024];
+
+            InetAddress ip = InetAddress.getLocalHost();
+            
+            //sending request to serv1
+            DatagramSocket sendingrequesttoserv1 = new DatagramSocket(serv1alwaysonport);
+            DatagramPacket sendreqtorserv1 = new DatagramPacket(senddata, senddata.length, ip, serv1alwaysonport);
+            sendingrequesttoserv1.send(sendreqtorserv1);
+            sendingrequesttoserv1.close();
+
+            //getting values from serv1
+            DatagramSocket gettingdatafromserv1 = new DatagramSocket(dataincoming);
+            DatagramPacket packetfromserv1 =new DatagramPacket(receivedataserv1,receivedataserv1.length);
+
+            gettingdatafromserv1.receive(packetfromserv1);
+            output = output + " "+ packetfromserv1.toString();
+
+            gettingdatafromserv1.close();
+            
+
+
+            //sending request to serv2
+            DatagramSocket sendingrequesttoserv2 = new DatagramSocket(serv2alwaysonport);
+            DatagramPacket sendreqtorserv2 = new DatagramPacket(senddata, senddata.length, ip, serv2alwaysonport);
+            sendingrequesttoserv2.send(sendreqtorserv2);
+            sendingrequesttoserv2.close();
+
+            //getting values from serv1
+            DatagramSocket gettingdatafromserv2 = new DatagramSocket(dataincoming);
+            DatagramPacket packetfromserv2 =new DatagramPacket(receivedataserv2,receivedataserv2.length);
+
+            gettingdatafromserv2.receive(packetfromserv2);
+            output = output + " "+ packetfromserv2.toString();
+            
+            gettingdatafromserv2.close();
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        return "ServerName: " + listallshows.toString() + " " + output;   
     }
     
     
@@ -192,5 +297,41 @@ public class ServATW extends UnicastRemoteObject implements RMIs {
             return "Invalid movie ID: Invalid date format";
         }
         return "Valid movie ID";
+    }
+    
+    //UDP
+    public static void listMovieServertoServer (String movieName, String serverrequest) throws RemoteException{
+        String output = "";
+        ArrayList<String> listallshows = new ArrayList<String>();
+        Map<String, Integer> tempMap = new HashMap<>(movies.get(movieName));
+        for (Map.Entry<String, Integer> entry : tempMap.entrySet()) {
+            listallshows.add(entry.getKey() + " with " + entry.getValue() + " capacity");
+        }
+        output =  ServerName +" : "+ listallshows.toString();
+        
+        try {
+            byte[] senddata = new byte[1024];
+            senddata = output.getBytes();
+            InetAddress ip = InetAddress.getLocalHost();
+            DatagramPacket sendpacket = new DatagramPacket(senddata, senddata.length, ip, 6999);
+            
+            if(serverrequest.equalsIgnoreCase("ATW"))
+            {
+                DatagramSocket toserv = new DatagramSocket(serv1dataincoming);
+                toserv.send(sendpacket);
+                toserv.close();
+            }
+            else if(serverrequest.equalsIgnoreCase("VER"))
+            {
+                //sending to the server that asked
+                DatagramSocket toserv = new DatagramSocket(serv2dataincoming);
+                toserv.send(sendpacket);
+                toserv.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+        
     }
 }
